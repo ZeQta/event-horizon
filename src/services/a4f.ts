fe79f602ff2835ecc83d4e1b5df679c2590b930a
ZeQta <playm23410@gmail.com>
2025-06-30 22:52:04 +0530

    Update a4f.ts

--- src/services/a4f.ts
+++ src/services/a4f.ts
@@ -23,7 +23,7 @@ export class A4FService {
           messages: messages,
           stream: true,
           temperature: 0.7,
-          max_tokens: 4000,
+          max_tokens: 200000,
         }),
       });
 
